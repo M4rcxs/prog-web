8cb0cfd25eaa487a0964a1c510ce57d257b70ed7
Kevyn1001 <kevyn_eduardo12@yahoo.com>
2024-09-25 21:40:24 -0300

    feat: lucid configuration

--- e-store/adonisrc.ts
+++ e-store/adonisrc.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   | will be scanned automatically from the "./commands" directory.
   |
   */
-  commands: [() => import('@adonisjs/core/commands')],
+  commands: [() => import('@adonisjs/core/commands'), () => import('@adonisjs/lucid/commands')],
 
   /*
   |--------------------------------------------------------------------------
@@ -34,6 +34,7 @@ export default defineConfig({
     () => import('@adonisjs/vite/vite_provider'),
     () => import('@adonisjs/shield/shield_provider'),
     () => import('@adonisjs/static/static_provider'),
+    () => import('@adonisjs/lucid/database_provider')
   ],
 
   /*
