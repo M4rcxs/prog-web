022eb721fb11b58d92f31fdb625f518e782bc28f
Kevyn1001 <kevyn_eduardo12@yahoo.com>
2024-09-27 21:45:52 -0300

    feat: add category table

--- e-store/database/migrations/1727482885857_create_categories_table.ts
+++ e-store/database/migrations/1727482885857_create_categories_table.ts
@@ -1,15 +1,13 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'products'
+  protected tableName = 'categories'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('name').notNullable()
-      table.text('description').nullable()
-      table.decimal('price', 12, 2).notNullable() 
-      table.text('image_url').notNullable()
+      table.text('description')
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
