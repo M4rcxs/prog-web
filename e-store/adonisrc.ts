a79b75f461a3ffae1ac4ae60aec8f5a1c32ec26a
Kevyn1001 <kevyn_eduardo12@yahoo.com>
2024-10-20 17:42:56 -0300

    fix: update on middleware auths

--- e-store/adonisrc.ts
+++ e-store/adonisrc.ts
@@ -34,7 +34,8 @@ export default defineConfig({
     () => import('@adonisjs/vite/vite_provider'),
     () => import('@adonisjs/shield/shield_provider'),
     () => import('@adonisjs/static/static_provider'),
-    () => import('@adonisjs/lucid/database_provider')
+    () => import('@adonisjs/lucid/database_provider'),
+    () => import('@adonisjs/auth/auth_provider')
   ],
 
   /*
