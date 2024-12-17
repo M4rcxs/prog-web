4b92b112b672a46eaf438a1f9d4ffb34a27a9643
Marcos Silva <marcos.201314@hotmail.com>
2024-12-17 18:02:20 -0300

    refact: changed to get instead post

--- e-store/start/routes.ts
+++ e-store/start/routes.ts
@@ -50,7 +50,7 @@ router
     router.post('/store', [AuthController, 'store']).as('store') // Rota para realizar o login
     router.get('/create', [AuthController, 'create']).as('create_user')
     router.get('/:id', [AuthController, 'show']).as('show').use(middleware.auth())
-    router.post('/userProfile', [AuthController, 'profile']).as('profile').use(middleware.auth())
+    router.get('/userProfile', [AuthController, 'profile']).as('profile').use(middleware.auth())
     router.delete('/:id', [AuthController, 'destroy']).as('destroy')
     router.patch('/:id', [AuthController, 'patch']).as('patch')
     router.post('/logout', [AuthController, 'logout']).as('logout').use(middleware.auth())
