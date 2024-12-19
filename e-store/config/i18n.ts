6c3dc6dd29b26b1fbe2b1cb387f2b54e52237755
Marcos Silva <marcos.201314@hotmail.com>
2024-12-18 22:22:03 -0300

    fix: update default locale to Portuguese and refine user creation translations

--- e-store/config/i18n.ts
+++ e-store/config/i18n.ts
@@ -2,7 +2,7 @@ import app from '@adonisjs/core/services/app'
 import { defineConfig, formatters, loaders } from '@adonisjs/i18n'
 
 const i18nConfig = defineConfig({
-  defaultLocale: 'en',
+  defaultLocale: 'pt',
   formatter: formatters.icu(),
 
   loaders: [
@@ -16,9 +16,10 @@ const i18nConfig = defineConfig({
      *   - "resources/lang/it"
      */
     loaders.fs({
-      location: app.languageFilesPath()
+      location: app.languageFilesPath(),
     }),
   ],
 })
+console.log(app.languageFilesPath())
 
 export default i18nConfig
